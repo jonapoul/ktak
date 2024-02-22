5e8983ed019d8648344ceb218bbc28e527fa6640
Jon Poulton <jpoulton@pm.me>
2024-02-22 22:28:38 +0000

    Don't exclude androidx packages

--- build-logic/src/main/kotlin/convention-atak.gradle.kts
+++ build-logic/src/main/kotlin/convention-atak.gradle.kts
@@ -2,30 +2,6 @@
 
 import org.gradle.accessors.dm.LibrariesForLibs
 
-configurations.configureEach {
-  // Exclude dependencies which are already included in the ATAK SDK jar
-  exclude(group = "androidx.activity", module = "activity")
-  exclude(group = "androidx.annotation", module = "annotation")
-  exclude(group = "androidx.annotation", module = "annotation-experimental")
-  exclude(group = "androidx.appcompat", module = "appcompat")
-  exclude(group = "androidx.arch.core", module = "core-common")
-  exclude(group = "androidx.arch.core", module = "core-runtime")
-  exclude(group = "androidx.collection", module = "collection")
-  exclude(group = "androidx.core", module = "core")
-  exclude(group = "androidx.customview", module = "customview")
-  exclude(group = "androidx.exifinterface", module = "exifinterface")
-  exclude(group = "androidx.fragment", module = "fragment")
-  exclude(group = "androidx.lifecycle", module = "lifecycle")
-  exclude(group = "androidx.lifecycle", module = "lifecycle-common")
-  exclude(group = "androidx.lifecycle", module = "lifecycle-livedata-core")
-  exclude(group = "androidx.lifecycle", module = "lifecycle-process")
-  exclude(group = "androidx.lifecycle", module = "lifecycle-runtime")
-  exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel")
-  exclude(group = "androidx.lifecycle", module = "lifecycle-viewmodel-savedstate")
-  exclude(group = "androidx.preference", module = "preference")
-  exclude(group = "androidx.savedstate", module = "savedstate")
-}
-
 val compileOnly by configurations
 val testImplementation by configurations
 
