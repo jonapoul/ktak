4a8903de7f3609e0a5dfbdd81698aa5f3afdb439
Jon Poulton <jpoulton@pm.me>
2024-02-26 21:07:36 +0000

    Generate module diagrams

--- README.md
+++ README.md
@@ -31,5 +31,9 @@ dependencies {
 }
 ```
 
+## Modules
+
+![project-dependency-graph.png](project-dependency-graph.png)
+
 ## TODO
 - Fix janky icons
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
   alias(libs.plugins.blueprint.atak.library) apply false
   alias(libs.plugins.blueprint.atak.plugin) apply false
   alias(libs.plugins.blueprint.detekt) apply false
-  alias(libs.plugins.blueprint.diagrams) apply false
   alias(libs.plugins.blueprint.ktlint) apply false
   alias(libs.plugins.blueprint.kotlin.android) apply false
   alias(libs.plugins.blueprint.licensee) apply false
@@ -17,6 +16,7 @@ plugins {
   alias(libs.plugins.blueprint.dependencyGuard)
   alias(libs.plugins.blueprint.dependencyAnalysis)
   alias(libs.plugins.blueprint.doctor)
+  alias(libs.plugins.blueprint.diagrams)
   alias(libs.plugins.blueprint.kover)
   alias(libs.plugins.blueprint.versions)
 }
