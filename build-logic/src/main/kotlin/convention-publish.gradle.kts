711d379db79b499f5a5d9eba77557910b94a97d0
Jon Poulton <jpoulton@pm.me>
2024-07-21 14:09:21 +0100

    Add explicit versions to BOM libs

--- build-logic/src/main/kotlin/convention-publish.gradle.kts
+++ build-logic/src/main/kotlin/convention-publish.gradle.kts
@@ -2,12 +2,3 @@ plugins {
   id("dev.jonpoulton.maven.publish")
   id("org.jetbrains.dokka")
 }
-
-publishing {
-  repositories {
-    maven {
-      name = "MavenLocalProper"
-      url = mavenLocal().url
-    }
-  }
-}
